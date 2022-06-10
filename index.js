f4f4068364a8a1b997f74340935d93c8b7311677
Sean Oh <ohsean812@gmail.com>
2022-06-10 14:24:09 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, event) {
+    const newArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        newArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    };
+    return newArray;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
